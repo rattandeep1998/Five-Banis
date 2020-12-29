48afbb075e6b47de48bd0e8c6027a3779579a194
RATTANDEEP SINGH <rattandeep_singh@intuit.com>
2020-12-29 14:46:07 +0530

    Initial Commit

---
+++ Five-Banis/Five_BanisApp.swift
@@ -0,0 +1,17 @@
+//
+//  Five_BanisApp.swift
+//  Five-Banis
+//
+//  Created by RATTANDEEP SINGH on 29/12/20.
+//
+
+import SwiftUI
+
+@main
+struct Five_BanisApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
